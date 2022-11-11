3ce8e89aca7fbc67300f5dcd3fcd0c8b3aaf2462
Azmatkhan12 <rehan.dgms@gmail.com>
2022-11-11 11:01:43 +0530

    add location

--- src/App.js
+++ src/App.js
@@ -3,35 +3,51 @@
  import ExpenseItem from './components/ExpenseItem';
 
 function App() {
+  
   let expense = [
     {
+      id: "exp1",
+      date: new Date(2022,11,10),
       expeseTitle: "food",
-      expeseAmount: 100,
+      expeseAmount: 8500,
+      explocation: "Begumpet",
     },
     {
-      expeseTitle: "petrol",
-      expeseAmount: 500,
+      id: "exp2",
+      date: new Date(2021,12,25),
+      expeseTitle: "Car Insurance",
+      expeseAmount: 4000,
+      explocation: "Dhanbad",
     },
     {
-      expeseTitle: "Movies",
-      expeseAmount: 800,
-    }
+      id: "exp3",
+      date: new Date(2022,6,10),
+      expeseTitle: "Room Rent",
+      expeseAmount: 4200,
+      explocation: "Old Custom Basti",
+    },
   ];
 
   return (
     <div>
       /<h1> Expense Tracker</h1>
       <ExpenseItem
+        date={expense[0].date}
         title={expense[0].expeseTitle}
         amount={expense[0].expeseAmount}
+        location={expense[0].explocation}
       ></ExpenseItem>
       <ExpenseItem
+        date={expense[1].date}
         title={expense[1].expeseTitle}
         amount={expense[1].expeseAmount}
+        location={expense[1].explocation}
       ></ExpenseItem>
       <ExpenseItem
+        date={expense[2].date}
         title={expense[2].expeseTitle}
         amount={expense[2].expeseAmount}
+        location={expense[2].explocation}
       ></ExpenseItem>
     </div>
   );
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,25 @@
-  import './ExpenseItem.css';
+/** @format */
 
-function ExpenseItem(props){
-    return (
-      <div className="Expense-item">
-        <div className="Expene-item_price">
-          <h3> {props.title}</h3>
-          <p> Rs{props.amount}</p>
-        </div>
-        <div></div>
+import "./ExpenseItem.css";
+function ExpenseItem (props) {
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
+
+  return (
+    <div className="Expense-item">
+      <div>
+        <div>{month}</div>
+        <div>{day}</div>
+        <div> {year}</div>
       </div>
-    );
+      <h2 className="h2">{props.title}</h2>
+      <div>location: {props.location}</div>
+      <div className="Expense-item_description">
+        <div className="Expene-item_price">${props.amount}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
